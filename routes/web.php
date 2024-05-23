26f297b77bd6873bf873088a09b9a0c536d6fc65
Arifurrahmanrifat29112002 <arifurrahmanrifat72@gmail.com>
2024-05-24 03:57:24 +0600

    dashboard page setup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Livewire\AdminLogin;
+use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,9 +20,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -34,6 +35,7 @@ Route::middleware('auth')->group(function () {
 Route::middleware('guest')->group(function () {
     /* admin login */
     Route::get('/dashboard/login',AdminLogin::class)->name('dashbord.login');
+    Route::get('/dashboard',Dashboard::class)->name('dashbord.show');
 });
 
 
